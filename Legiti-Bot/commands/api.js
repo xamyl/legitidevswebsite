b797ebe48b739d43118a2a4c46e8d226e6542629
Omri H <omri.hermon@gmail.com>
2024-11-30 11:44:37 +0200

    fancy embed

--- Legiti-Bot/commands/api.js
+++ Legiti-Bot/commands/api.js
@@ -1,12 +1,16 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("api")
-    .setDescription("API"),
+    .setDescription("Api command"),
   async execute(interaction) {
     try {
-      await interaction.reply("<@" + `${interaction.user.id}` + ">");
+      const apiEmbed = new EmbedBuilder()
+        .setColor("Blue")
+        .setTitle("Api")
+        .setDescription("this is the api command");
+      await interaction.reply({embeds: [apiEmbed]});
     } catch (error) {
       await interaction.reply(`${error}`);
     }

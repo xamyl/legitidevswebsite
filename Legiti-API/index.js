497f327e9b2d314684218b366f7d8eaf28c06760
Omri H <omri.hermon@gmail.com>
2024-11-30 16:51:28 +0200

    no redirect

--- Legiti-API/index.js
+++ Legiti-API/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const config = require("./config.json");
+const path = require("node:path");
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => { 
-    res.redirect("/home.html")
-}); 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.get("/browse", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "world-browser.html"));
+});
 
 app.listen(config.port, () => {
-    console.log(`Running on port ${config.port}`);
+  console.log(`Running on port ${config.port}`);
 });

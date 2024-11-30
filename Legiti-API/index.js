b71ddc48cdd1885440935127dafaca7591896e31
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-11-30 23:59:51 +0800

    added navbar to all pages + api page

--- Legiti-API/index.js
+++ Legiti-API/index.js
@@ -2,32 +2,35 @@ import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 import config from "./config.json" with { type: "json" };
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const fastify = Fastify({
-  logger: true
-})
+    logger: false,
+});
 
 fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'public'),
+    root: path.join(__dirname, "public"),
 });
 
 fastify.get("/", function (req, res) {
-  res.sendFile('home.html');
+    res.sendFile("home.html");
 });
 
 fastify.get("/browse", function (req, res) {
-  res.sendFile('world-browser.html');
+    res.sendFile("world-browser.html");
+});
+
+fastify.get("/api", function (req, res) {
+    res.sendFile("api.html");
 });
 
 fastify.listen({ port: config.port }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  // Server is now listening on ${address}
-})
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+});

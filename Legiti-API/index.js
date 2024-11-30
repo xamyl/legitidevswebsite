9c0dd84558f834105273675b7f51b6cb932968cf
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-11-30 21:48:37 +0800

    CSS updates + Folder structure changes

--- Legiti-API/index.js
+++ Legiti-API/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => { 
+    res.redirect("/home.html")
+}); 
+
 app.listen(config.port, () => {
     console.log(`Running on port ${config.port}`);
 });

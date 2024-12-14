de74de2b46b3be79a9431a25f6724be821245047
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-12-14 17:04:03 +0800

    temp test

--- src/routes/browse/[world]/+page.server.js
+++ src/routes/browse/[world]/+page.server.js
@@ -1,5 +1,12 @@
-export const load = ({ params }) => {
-    return {
-        uuid: params.world
+export const load = async ({ params }) => {
+    const res = await fetch(`https://api.omrih.me/world/${params.world}`)
+    const json = await res.json()
+
+    if (!json) return {
+        name: "wuh u doin",
+        description: "give me a world not some random thing",
+        world_uuid: "this aint valid",
     }
+
+    return json
 }
--- src/routes/browse/[world]/+page.svelte
+++ src/routes/browse/[world]/+page.svelte
@@ -2,4 +2,18 @@
     let { data } = $props();
 </script>
 
-<h1>{data.uuid}</h1>
+<div class="main-container">
+    <h1>{data.name}</h1>
+    <h2>{data.description}</h2>
+    <p>{data.world_uuid}</p>
+</div>
+
+<style>
+    .main-container {
+        display: flex;
+		flex-direction: column;
+		background-color: light-dark(var(--main-light), var(--main-dark));
+		min-height: 100vmin;
+		align-items: center;
+    }
+</style>

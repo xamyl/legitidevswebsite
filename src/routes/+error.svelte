3f03ad8207c2cc3c7efab3f47fa3ca8374b5d9f8
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-12-14 18:22:41 +0800

    add error handlin

---
+++ src/routes/+error.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { page } from "$app/stores";
+    
+    const ERROR_MESSAGES = [
+        "Error!",
+        "i'm 90% sure that this is not my fault and its yours probably i think.",
+        "what did you do this time",
+        "actually maybe its our fault? idk.",
+        "fakekdeer is here",
+        "we all know koori did this",
+        "can you suggest more messages here?",
+    ]
+
+    const PICKED_ERROR_MESSAGE = ERROR_MESSAGES[Math.floor(Math.random() * ERROR_MESSAGES.length)]
+</script>
+
+<div class="main-container">
+    <img src="https://cdn.discordapp.com/emojis/778385740236193842.webp?size=96&animated=true" alt="evilreefgif">
+    <h1>Oh no! <code>{$page.status} {$page.error.message}</code></h1>
+    <h2>{PICKED_ERROR_MESSAGE}</h2>
+</div>
+
+<style>
+    h1, h2 {
+        max-width: 80vw;
+    }
+
+    img {
+        height: auto;
+        width: 200px;
+    }
+
+    .main-container {
+        display: flex;
+		flex-direction: column;
+		background-color: light-dark(var(--main-light), var(--main-dark));
+		min-height: 100vmin;
+		align-items: center;
+        justify-content: center;
+    }
+</style>
--- src/routes/browse/[world]/+page.server.js
+++ src/routes/browse/[world]/+page.server.js
@@ -1,12 +1,10 @@
+import { error } from '@sveltejs/kit'
+
 export const load = async ({ params }) => {
     const res = await fetch(`https://api.omrih.me/world/${params.world}`)
     const json = await res.json()
 
-    if (!json) return {
-        name: "wuh u doin",
-        description: "give me a world not some random thing",
-        world_uuid: "this aint valid",
-    }
+    if (!json) error(404, { message: `Invalid world UUID.` })
 
     return json
 }

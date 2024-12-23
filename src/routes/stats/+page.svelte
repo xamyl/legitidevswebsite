ea9fa7bb8106ac485c3fe4721456e2a02912a349
Anjo2807 <dmytro.lopatyn@gmail.com>
2024-12-23 12:52:21 +0000

    changed the error text

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -21,7 +21,7 @@
             try {
                 console.log(fetchtop10(numrn))
             } catch {
-                console.log("THIS ISN'T WORKING I WANT TO CONNECT A FORK TO A FUCKING WALL SOCKET")
+                console.log("smth happened idk")
             }
             
         }

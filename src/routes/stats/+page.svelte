4eae65ea7568bdeca5f0ad5dd1f0660010482c85
Trplnr <87922281+Trioplane@users.noreply.github.com>
2024-12-24 18:01:53 +0800

    make this work

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,37 +1,23 @@
 
 <script>
-    import { json } from "@sveltejs/kit";
     import { onMount } from "svelte";
-    
-    onMount(() => {
-        function fetchtop10(num) {
-            fetch('https://api.omrih.me/top/${num}')
-            .then((response) => {
-                return response.json()
-            })
-            .then ((data) => {
-                let top = data
+    let top10Worlds = $state([]);
 
-                let top_processed = '${top.name}'
-                return top_processed
-            })
-        }
+    async function fetchTop10() {
+        const res = await fetch("https://api.omrih.me/top/10");
+        const worlds = await res.json()
+        top10Worlds = worlds;
+    }    
 
-        for(var numrn = 1; numrn < 11; numrn++) {
-            try {
-                console.log(fetchtop10(numrn))
-            } catch {
-                console.log("smth happened idk")
-            }
-            
-        }
-    })
+    onMount(async () => { await fetchTop10() })
 </script>
 
 <div class="main-container">
     <h1>Legitimoose Stats</h1>
     <h2>Top 10 Worlds</h2>
     <ul id="top10">
-        
+        {#each top10Worlds as world}
+            <div>{world.name}</div>
+        {/each}
     </ul>
 </div>

f8a1bd5eccf8a9496785d9fba9561e487209e795
Anjo2807 <dmytro.lopatyn@gmail.com>
2024-12-24 11:11:39 +0100

    yea i dont even know

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,6 +1,7 @@
 
 <script>
-<<<<<<< HEAD
+    import { onMount } from "svelte";
+
     let top10Worlds = $state([]);
     
     async function fetchTop10() {
@@ -12,17 +13,6 @@
       top10Worlds = worlds
     }
     
-=======
-    import { onMount } from "svelte";
-    let top10Worlds = $state([]);
-
-    async function fetchTop10() {
-        const res = await fetch("https://api.omrih.me/top/10");
-        const worlds = await res.json()
-        top10Worlds = worlds;
-    }    
-
->>>>>>> 4eae65ea7568bdeca5f0ad5dd1f0660010482c85
     onMount(async () => { await fetchTop10() })
 </script>
     
@@ -32,13 +22,5 @@
 <div class="main-container">
     <h1>Legitimoose Stats</h1>
     <h2>Top 10 Worlds</h2>
-<<<<<<< HEAD
     
-=======
-    <ul id="top10">
-        {#each top10Worlds as world}
-            <div>{world.name}</div>
-        {/each}
-    </ul>
->>>>>>> 4eae65ea7568bdeca5f0ad5dd1f0660010482c85
 </div>

92006c2c936dbeb03cb671904f31830722d3f698
Hazle <xendyex@tutanota.com>
2025-01-21 19:49:14 +0000

    Wait I forgot one

--- src/routes/team/+page.js
+++ src/routes/team/+page.js
@@ -1,10 +1,7 @@
 export const load = async ({ fetch }) => {
-    const res = await fetch(`/uptime`)
-    const uptime = await res.json()
     return {
         page: {
             title: "Meet The Team"
         },
-        uptimes: uptime
     }
 }
